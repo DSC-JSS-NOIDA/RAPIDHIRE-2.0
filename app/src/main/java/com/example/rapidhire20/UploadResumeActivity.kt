a1e631acc6fc2bd48bed1e2e2c7ae7d66c2839f8
Shagun <shagunchahar8@gmail.com>
2023-09-28 17:28:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rapidhire20/UploadResumeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rapidhire20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UploadResumeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload_resume)
+    }
+}
