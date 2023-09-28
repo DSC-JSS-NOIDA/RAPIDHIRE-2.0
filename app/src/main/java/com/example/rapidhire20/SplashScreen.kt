256f112885e8eb91feb5fb85200a1730295f49e9
AbhiSharma096 <Abhisheksharma10a@gmail.com>
2023-09-28 18:26:27 +0530

    my commit

--- app/src/main/java/com/example/rapidhire20/SplashScreen.kt
+++ app/src/main/java/com/example/rapidhire20/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.rapidhire20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,6 +8,14 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        val handler = android.os.Handler()
+
+
+            handler.postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
 
     }
 }
