72a0b5d4d282b7976ee63c95bde83c22d3df814c
AbhiSharma096 <Abhisheksharma10a@gmail.com>
2023-09-29 14:20:54 +0530

    Lottie animator is saved in Assets folder

--- app/src/main/java/com/example/rapidhire20/SplashScreen.kt
+++ app/src/main/java/com/example/rapidhire20/SplashScreen.kt
@@ -1,10 +1,12 @@
 package com.example.rapidhire20
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashScreen : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
